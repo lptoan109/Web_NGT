651e3c818a74250934a0843d161f559e45226aba
LPToan <lephuoctoan6411@gmail.com>
2025-09-22 00:18:39 +0700

    update

--- app.py
+++ app.py
@@ -43,6 +43,11 @@ cloudinary.config(
 
 # Khởi tạo các extension
 db = SQLAlchemy(app)
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 bcrypt = Bcrypt(app)
 mail = Mail(app)
 login_manager = LoginManager(app)
